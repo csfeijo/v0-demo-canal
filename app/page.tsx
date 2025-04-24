39342d9611f74928e80b589bd0fcf83eb051eb91
Cícero Feijó <cicero.feijo@gmail.com>
2025-04-24 01:06:48 -0300

    feat: alterado nome na pagina inicial

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
               <div className="flex flex-col justify-center space-y-4">
                 <div className="space-y-2">
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                    Olá, eu sou <span className="text-emerald-400">Cícero Feijó</span>
+                    Eu sou o <span className="text-emerald-400">Professor Feijó</span>
                   </h1>
                   <p className="max-w-[600px] text-muted-foreground md:text-xl">
                     Sou um profissional especializado em Desenvolvimento Front-end. Crio vídeos, escrevo artigos e ofereço treinamentos
